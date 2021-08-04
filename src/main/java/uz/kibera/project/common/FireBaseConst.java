b3333b173572b6481c21397677da8805bcd32efa
shox1998 <shohruhmirzo2017@mail.ru>
2021-08-04 22:48:05 +0500

    Added static image for Notices

--- src/main/java/uz/kibera/project/common/FireBaseConst.java
+++ src/main/java/uz/kibera/project/common/FireBaseConst.java
@@ -5,5 +5,5 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class FireBaseConst {
     public static final String FIREBASE_TOKEN_CONST = "chUQFITZkKc:APA91bGWlT-7hRr9Acjq6joprPcnFGWEbSpljCs7dwLALfM0Zh6cg7E6jOgdf2ZF8TCq9nPP5TdiAK1ih9n-A5PYC-S-iVulIsUdxARblBp3PmPPI3i8lG2GODfLjAiHIzfMi2XemkZO";
-    public static final String STATIC_IMAGE_URL="https://commons.bmstu.wiki/images/5/59/Spring-boot-logo.png";
+    public static final String STATIC_IMAGE_URL="https://hhcdn.ru/employer-logo/3358041.png";
 }
